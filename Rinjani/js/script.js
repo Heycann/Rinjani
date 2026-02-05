e82b47547e73bab1fbff7edb0f31f9a8a93e3e81
Heycann <161898163+Heycann@users.noreply.github.com>
2026-02-06 06:00:54 +0700

    Add files via upload

--- Rinjani/js/script.js
+++ Rinjani/js/script.js
@@ -1,5 +1,8 @@
 // TOGGLE CLASS ACTIVE
 const navbarNav = document.querySelector(".navbar-nav");
+const menuIcon = document.querySelector("#menu-icon");
+
+feather.replace();
 
 // KETIKA HAMBURGER MENU DIKLIK
 document.querySelector("#menu-icon").onclick = () => {
@@ -14,3 +17,25 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
   }
 });
+const navbar = document.querySelector("navbar");
+window.onscroll = () => {
+  if (window.scrolly > 100) {
+    navbar.classList.add("navbar-scrolled");
+  } else {
+    navbar.classList.remove("navbar-scrolled");
+  }
+};
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("active");
+      }
+    });
+  },
+  {
+    threshold: 0.1,
+  },
+);
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
